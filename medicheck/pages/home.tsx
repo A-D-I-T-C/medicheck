b617f72d493a4f5d13297b57c31947183068015d
amn-xyz <amaan2@ualberta.ca>
2025-02-16 14:31:03 -0700

    feat: Basic routing possibility

---
+++ medicheck/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to MediCheck</h1>
+            <p>Your health, our priority.</p>
+        </div>
+    );
+};
+
+export default Home;
